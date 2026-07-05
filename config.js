38825454d224ea5d5f0e90b34b120b1c4c69e442
bluetmobcompany-glitch <bluetmobcompany@gmail.com>
2026-07-05 17:17:41 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// Paste your Google Apps Script Web App URL here after deploying Code.gs.
+// It looks like: https://script.google.com/macros/s/AKfycb.../exec
+const API_URL = "https://script.google.com/macros/s/AKfycbxUU1WlN1hLiHFdVnTBi0NFAY4qo-YkCOTzX_5_eFleXLjV3r22VdSmceBDSjAqTZwf/exec";
